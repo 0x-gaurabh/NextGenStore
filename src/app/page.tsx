5233dc333194bd537e86c806f66d2ee63b689fb3
unknown <sahu29558@gmail.com>
2024-11-30 13:19:14 +0530

    Updated file to fix deployment issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,11 @@
 "use client";
-
-import { fetchProduct } from "@/redux/slices/productSlice";
-import { AppDispatch, RootState } from "@/redux/store";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import Section from "@/components/Section";
 import { useAccount } from "wagmi";
 import { useRouter } from "next/navigation";
 
-export default function Home() {
-  const dispatch = useDispatch<AppDispatch>();
-
-  interface Product {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    category: string;
-    image: string;
-    rating: {
-      rate: number;
-      count: number;
-    };
-  }
-
-  const { products, loading, error } = useSelector((state: RootState) => state.product);
-  const [product, setProduct] = useState<Product[] | null>(null);
-
-  // Fetch products on component mount
-  useEffect(() => {
-    dispatch(fetchProduct());
-  }, [dispatch]);
-
-  // Sync Redux products to local state
-  useEffect(() => {
-    if (products) {
-      setProduct(products);
-    }
-  }, [products]);
+export default function Home() {  
+  
 
   const { isConnected } = useAccount();
   const router = useRouter();
--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -2,6 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+const loadLocalStorage =() =>{
+  try{
+      const add=localStorage.getItem('cart')
+      return add ? JSON.parse(add) : []
+  } 
+  catch{
+      console.warn('Could not load data frm local storage');
+      return [];
+  } 
+}
+
+const saveToLocalStorage =(cart :any) => {
+  try{
+    const add=JSON.stringify(cart)
+    localStorage.setItem('cart' ,add)
+  }
+  catch{
+    console.warn("Could not save storgae dataa");
+  }
+}
+
+
 interface CartItem {
   id: number;
   qty: number;
@@ -16,7 +38,7 @@ interface cartProps {
 }
 
 const initialState: cartProps = {
-  cart: [],
+  cart: loadLocalStorage(),
 };
 
 const CartSlice = createSlice({
@@ -35,6 +57,7 @@ const CartSlice = createSlice({
       } else {
         state.cart.push(action.payload);
       }
+      saveToLocalStorage(state.cart)
 
       toast.success("Item added to cart", {
         position: "top-right",
@@ -51,6 +74,7 @@ const CartSlice = createSlice({
       state.cart = state.cart.filter(
         (item: any) => item.id !== action.payload.id
       );
+      saveToLocalStorage(state.cart)
       toast.success(`Item removed succesfully`, {
         position: "top-right",
         autoClose: 500,
@@ -67,11 +91,15 @@ const CartSlice = createSlice({
       state.cart = state.cart.map((item: any) =>
         item.id === action.payload.id ? { ...item, qty: item.qty + 1 } : item
       );
+      saveToLocalStorage(state.cart)
+
     },
     decrement: (state, action) => {
       state.cart = state.cart.map((item: any) =>
         item.id === action.payload.id ? { ...item, qty: item.qty - 1 } : item
       );
+      saveToLocalStorage(state.cart)
+
     },
   },
 });
